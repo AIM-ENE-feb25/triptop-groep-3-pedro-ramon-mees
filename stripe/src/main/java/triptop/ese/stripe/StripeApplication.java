7d2353546a871515ce8c77d38d33f972f8d3de7d
Mees van Aarsen <meesvanaarsen@gmail.com>
2025-03-20 22:38:40 +0100

    feat: untested code :(

--- stripe/src/main/java/triptop/ese/stripe/StripeApplication.java
+++ stripe/src/main/java/triptop/ese/stripe/StripeApplication.java
@@ -1,6 +1,8 @@
 package triptop.ese.stripe;
 
 import com.stripe.exception.StripeException;
+import com.stripe.model.Customer;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -24,6 +26,8 @@ public class StripeApplication {
     private void runStripeTest() {
         try {
             stripeTesting.createProduct("Test Product", "This is a test product", "EUR", 2000L);
+            Customer c = stripeTesting.createCustomer("Test Customer", "test@test.com");
+            stripeTesting.createInvoice(c.getId());
         } catch (StripeException e) {
             throw new RuntimeException(e);
         }
--- stripe/src/main/java/triptop/ese/stripe/StripeTesting.java
+++ stripe/src/main/java/triptop/ese/stripe/StripeTesting.java
@@ -2,13 +2,15 @@ package triptop.ese.stripe;
 
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
+import com.stripe.model.Customer;
+import com.stripe.model.Invoice;
 import com.stripe.model.Price;
 import com.stripe.model.Product;
+import com.stripe.param.CustomerCreateParams;
+import com.stripe.param.InvoiceCreateParams;
 import com.stripe.param.PriceCreateParams;
 import com.stripe.param.ProductCreateParams;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -18,30 +20,55 @@ public class StripeTesting {
     private String apiKey;
 
     // Of-course would prefer an enum for currency here
-    public void createProduct(String name, String description, String currency, Long price) throws StripeException {
+    public Product createProduct(String name, String description, String currency, Long price) throws StripeException {
         Stripe.apiKey = apiKey;
 
-        ProductCreateParams productParams =
-                ProductCreateParams.builder()
-                        .setName(name)
-                        .setDescription(description)
-                        .build();
+        ProductCreateParams productParams = ProductCreateParams.builder()
+                .setName(name)
+                .setDescription(description)
+                .build();
 
         Product productObj = Product.create(productParams);
 
         System.out.println("Product created: " + productObj.getId());
 
-        PriceCreateParams priceParams =
-                PriceCreateParams.builder()
-                        .setProduct(productObj.getId())
-                        .setCurrency(currency)
-                        .setUnitAmount(price)
-                        .build();
+        PriceCreateParams priceParams = PriceCreateParams.builder()
+                .setProduct(productObj.getId())
+                .setCurrency(currency)
+                .setUnitAmount(price)
+                .build();
 
         Price priceObj = Price.create(priceParams);
 
         System.out.println("Price created: " + priceObj.getId() + " for product: " + priceObj.getProduct());
+        return productObj;
     }
 
+    public Customer createCustomer(String name, String email) throws StripeException {
+        Stripe.apiKey = apiKey;
+
+        CustomerCreateParams params = CustomerCreateParams.builder()
+                .setName(name)
+                .setEmail(email)
+                .build();
+
+        Customer customer = Customer.create(params);
+
+        System.out.println("Customer created: " + customer.getId());
+        return customer;
+    }
+
+    public Invoice createInvoice(String customerID) throws StripeException {
+        Stripe.apiKey = apiKey;
+
+        InvoiceCreateParams invoiceParams = InvoiceCreateParams.builder()
+                .setCustomer(customerID)
+                .build();
+
+        Invoice invoice = Invoice.create(invoiceParams);
+
+        System.out.println("Invoice created: " + invoice.getId());
+        return invoice;
+    }
 
 }
