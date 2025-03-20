da2b5dcc4b6f1f5a3569f9fcd02b386c44e06067
Mees van Aarsen <M.vanAarsen@student.han.nl>
2025-03-20 19:34:40 +0100

    feat: init setup for prototype project

---
+++ stripe/src/test/java/triptop/ese/stripe/StripeApplicationTests.java
@@ -0,0 +1,13 @@
+package triptop.ese.stripe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StripeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
