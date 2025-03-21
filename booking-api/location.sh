451a1399ef2ec50c0264fb13ad1a9718ea5a61d3
Ramon Bijl <snc19917@gmail.com>
2025-03-21 10:05:22 +0100

    Improve shell scripts

--- booking-api/location.sh
+++ booking-api/location.sh
@@ -1,4 +1,9 @@
+HOST='booking-com15.p.rapidapi.com'
+KEY="47aa077e79msh3aa641c74b24ecep1c9291jsned53c1e5db74"
+
+CITY="arnhem"
+
 curl -X GET \
-	'https://booking-com15.p.rapidapi.com/api/v1/hotels/searchDestination?query=arnhem' \
-	-H 'x-rapidapi-host: booking-com15.p.rapidapi.com' \
-	-H 'x-rapidapi-key: 47aa077e79msh3aa641c74b24ecep1c9291jsned53c1e5db74'
+	"https://$HOST/api/v1/hotels/searchDestination?query=$CITY" \
+	-H "x-rapidapi-host: $HOST" \
+	-H "x-rapidapi-key: $KEY"
--- booking-api/search.sh
+++ booking-api/search.sh
@@ -1,4 +1,11 @@
+HOST='booking-com15.p.rapidapi.com'
+KEY="47aa077e79msh3aa641c74b24ecep1c9291jsned53c1e5db74"
+
+START_DATE="2025-03-24"
+END_DATE="2025-03-25"
+
 curl -X GET \
-	'https://booking-com15.p.rapidapi.com/api/v1/hotels/searchHotels?dest_id=-2140612&search_type=CITY&arrival_date=2025-03-24&departure_date=2025-03-25&adults=1&children_age=0%2C17&room_qty=1&page_number=1&units=metric&temperature_unit=c&languagecode=nl&currency_code=EUR' \
-	-H 'x-rapidapi-host: booking-com15.p.rapidapi.com' \
-	-H 'x-rapidapi-key: 47aa077e79msh3aa641c74b24ecep1c9291jsned53c1e5db74'
+	"https://$HOST/api/v1/hotels/searchHotels?dest_id=-2140612&search_type=CITY&arrival_date=$START_DATE&departure_date=$END_DATE&adults=1&children_age=0%2C17&room_qty=1&page_number=1&units=metric&temperature_unit=c&languagecode=nl&currency_code=EUR" \
+	-H "x-rapidapi-host: $HOST" \
+	-H "x-rapidapi-key: $KEY"
+
