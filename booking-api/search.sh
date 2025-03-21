c369afb88f2a5331c55ab0f7283596105dd8d3d2
Ramon Bijl <snc19917@gmail.com>
2025-03-21 09:51:40 +0100

    Update key

--- booking-api/search.sh
+++ booking-api/search.sh
@@ -1,4 +1,4 @@
 curl -X GET \
 	'https://booking-com15.p.rapidapi.com/api/v1/hotels/searchHotels?dest_id=-2140612&search_type=CITY&arrival_date=2025-03-24&departure_date=2025-03-25&adults=1&children_age=0%2C17&room_qty=1&page_number=1&units=metric&temperature_unit=c&languagecode=nl&currency_code=EUR' \
 	-H 'x-rapidapi-host: booking-com15.p.rapidapi.com' \
-	-H 'x-rapidapi-key: 3ab405fed0msh0d94904ed446f58p155f23jsn6fc0bfb7477e'
+	-H 'x-rapidapi-key: 47aa077e79msh3aa641c74b24ecep1c9291jsned53c1e5db74'
