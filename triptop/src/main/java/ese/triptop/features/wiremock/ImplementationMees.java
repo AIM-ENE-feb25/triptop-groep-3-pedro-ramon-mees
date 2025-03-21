5ac7bbf3acc14e1ffdb5a1ec2e3d8698eb9aca60
Mees van Aarsen <M.vanAarsen@student.han.nl>
2025-03-21 13:33:05 +0100

    refactor: changed from wiremock to triptop

---
+++ triptop/src/main/java/ese/triptop/features/wiremock/ImplementationMees.java
@@ -0,0 +1,58 @@
+package ese.triptop.features.wiremock;
+
+import kong.unirest.core.HttpResponse;
+import kong.unirest.core.Unirest;
+import kong.unirest.core.UnirestException;
+import kong.unirest.core.json.JSONObject;
+
+public class ImplementationMees {
+
+    public void run() {
+        try {
+            HttpResponse<String> response = doLogin("edevries", "3g2Rw9sT1x");
+
+            System.out.println("||||||||||||||||||||");
+            System.out.println("Authentication response:");
+            System.out.println(response.getBody());
+            System.out.println("||||||||||||||||||||");
+
+            if (response.getStatus() == 201) {
+                JSONObject responseBody = new JSONObject(response.getBody());
+
+                HttpResponse<String> authResponse = doAuthorisation(
+                        "edevries",
+                        responseBody.getJSONObject("token").getString("value")
+                );
+
+                System.out.println("||||||||||||||||||||");
+                System.out.println("Authorization response:");
+                System.out.println(authResponse.getBody());
+                System.out.println("||||||||||||||||||||");
+            }
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private HttpResponse<String> doLogin(String username, String password) throws UnirestException {
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("username", username);
+        requestBody.put("password", password);
+
+        return Unirest.post("https://triptop-identity.wiremockapi.cloud/login")
+                .header("Content-type", "application/json")
+                .body(requestBody.toString())
+                .asString();
+    }
+
+    private HttpResponse<String> doAuthorisation(String username, String token) throws UnirestException {
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("username", username);
+        requestBody.put("application", "triptop");
+
+        return Unirest.post("https://triptop-identity.wiremockapi.cloud/checkAppAccess?token=" + token)
+                .header("Content-type", "application/json")
+                .body(requestBody.toString())
+                .asString();
+    }
+}
---
+++ triptop/src/main/java/ese/triptop/features/wiremock/WiremockTesting.java
@@ -0,0 +1,23 @@
+package ese.triptop.features.wiremock;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class WiremockTesting {
+
+    public void run() {
+        System.out.println("Wiremock testing");
+        // Create an instance of the implementation class and call its method
+        ImplementationMees implementationMees = new ImplementationMees();
+        implementationMees.run();
+
+        ImplementationRamon implementationRamon = new ImplementationRamon();
+        implementationRamon.run();
+
+        ImplementationPedro implementationPedro = new ImplementationPedro();
+        implementationPedro.run();
+        implementationPedro.getFlightOffers("ITF","RTR", "2024-02-01Z",7);
+
+
+    }
+}
