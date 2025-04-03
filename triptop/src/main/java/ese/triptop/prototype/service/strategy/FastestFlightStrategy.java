56c3576382bb7001690a6674ed78a8108594f6a0
PedrovD <pedro_vandouveren@hotmail.com>
2025-04-03 23:27:30 +0200

    Create FastestFlightStrategy.java
    defined the fastest strategy

---
+++ triptop/src/main/java/ese/triptop/prototype/service/strategy/FastestFlightStrategy.java
@@ -0,0 +1,19 @@
+package ese.triptop.prototype.service.strategy;
+import ese.triptop.prototype.domain.Flight;
+import ese.triptop.prototype.service.strategy.contracts.IFlightSearchStrategy;
+
+import java.util.List;
+import java.util.Comparator;
+
+import org.springframework.stereotype.Component;
+
+@Component("fastestFlightStrategy")
+public class FastestFlightStrategy implements IFlightSearchStrategy {
+    @Override
+    public Flight findBestFlight(List<Flight> availableFlights) {
+         System.out.println("Strategy: Applying Fastest Flight Strategy.");
+         return availableFlights.stream()
+                .min(Comparator.comparingInt(Flight::getArrivalAirport))
+                .orElse(null);
+    }
+}
